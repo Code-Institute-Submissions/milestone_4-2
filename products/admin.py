1be877af33c379b61644f7be9f9487f365ab85a6
heathergi85 <heathergi85@gmail.com>
2020-07-13 09:27:48 +0000

    products amended

--- products/admin.py
+++ products/admin.py
@@ -2,15 +2,4 @@ from django.contrib import admin
 from .models import Training
 
 # Register your models here.
-
-class ProductAdmin(admin.ModelAdmin):
-    list_display = (
-        "name",
-        "price",
-        "features",
-        "image",
-    )
-
-    ordering = ("pk",)
-
-admin.site.register(Training, ProductAdmin)
+admin.site.register(Training)
--- products/models.py
+++ products/models.py
@@ -1,34 +1,31 @@
 from django.db import models
 
-SESSIONS_FEATURES = [
-    ('free_initial_15_minute_consult', 'Free Initial 15 Minute Consultation'),
-    ('one_hour_of_games_based_training', 'One Hour of Games Based Training'),
-    ('three_hours_of_games_based_training', 'Three Hours of Games Based Training'),
-    ('five_hours_of_games_based_training', 'Five Hours of Games Based Training'),
-    ('detailed_training_plan', 'Detailed Training Plan'),
-    ('follow_up_phonecall', 'Follow Up Phonecall'),
-    ('access_to_private_facebook_group', 'Access to Private Facebook Group'),
-]   
 
+PRODUCT_INCLUDED_FEATURES_CHOICES = [
+    ('free_initial', 'Free Initial 15 Minute Consult'),
+    ('one_hour', 'One Hour of Games Based Training Sessions'),
+    ('three_hours', 'Three Hours of Games Based Training Sessions'),
+    ('five_hours', 'Five Hours of Games Based Training Sessions'),
+    ('training_plan', 'Detailed Training Plan'),
+    ('follow_up', 'Follow Up Phone Call'),
+    ('facebook_group', 'Access To Private Facebook Training Group'),
+]
 
 # Create your models here.
 
 class Training(models.Model):
-
-    class Meta:
-        verbose_name_plural = 'Training Sessions'
-        
-    name = models.CharField(max_length=254)
-    price = models.DecimalField(max_digits=6, decimal_places=2)
-    features = models.TextField(choices=SESSIONS_FEATURES)
+    
+    name = models.CharField(max_length=254, default='')
+    price = models.DecimalField(max_digits=6, decimal_places=2,)
+    features = models.TextField(choices=PRODUCT_INCLUDED_FEATURES_CHOICES)
     image = models.ImageField(upload_to='media/products', null=True, blank=True)
 
     def __str__(self):
         return self.name
 
-    def get_features(self):
+    def get_features_display_list(self):
         feature_list = []
-        d = dict(SESSIONS_FEATURES)
+        d = dict(PRODUCT_INCLUDED_FEATURES_CHOICES)
         for feature in self.features:
             if feature in d:
                 feature_list.append(d[feature])
