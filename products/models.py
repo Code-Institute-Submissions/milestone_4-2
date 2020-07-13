5b818117c53a2b3f90bf828d89a9b6dc8ecf0bbe
heathergi85 <heathergi85@gmail.com>
2020-07-13 20:51:11 +0000

    basic product page setup

--- products/models.py
+++ products/models.py
@@ -1,8 +1,9 @@
 from django.db import models
+from multiselectfield import MultiSelectField
 
 
-PRODUCT_INCLUDED_FEATURES_CHOICES = [
-    ('free_initial', 'Free Initial 15 Minute Consult'),
+SESSION_FEATURES = [
+    ('free_initial', 'Free 15 Minute Initial Consult'),
     ('one_hour', 'One Hour of Games Based Training Sessions'),
     ('three_hours', 'Three Hours of Games Based Training Sessions'),
     ('five_hours', 'Five Hours of Games Based Training Sessions'),
@@ -13,19 +14,20 @@ PRODUCT_INCLUDED_FEATURES_CHOICES = [
 
 # Create your models here.
 
+
 class Training(models.Model):
-    
+
     name = models.CharField(max_length=254, default='')
     price = models.DecimalField(max_digits=6, decimal_places=2,)
-    features = models.TextField(choices=PRODUCT_INCLUDED_FEATURES_CHOICES)
-    image = models.ImageField(upload_to='media/products', null=True, blank=True)
+    features = MultiSelectField(choices=SESSION_FEATURES)
+    image = models.ImageField(upload_to='media/products')
 
     def __str__(self):
         return self.name
 
-    def get_features_display_list(self):
+    def get_features(self):
         feature_list = []
-        d = dict(PRODUCT_INCLUDED_FEATURES_CHOICES)
+        d = dict(SESSION_FEATURES)
         for feature in self.features:
             if feature in d:
                 feature_list.append(d[feature])
--- products/views.py
+++ products/views.py
@@ -1,3 +1,14 @@
 from django.shortcuts import render
+from .models import Training
+
 
 # Create your views here.
+def all_products(request):
+    """ A view to show products """
+
+    products = Training.objects.all().order_by('pk')
+
+    context = {
+        'products': products,
+    }
+    return render(request, "products/products.html", context)
