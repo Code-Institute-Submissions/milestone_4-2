36e3ea16d5210b24f71382205aa5a0486654f4c4
heathergi85 <heathergi85@gmail.com>
2020-07-21 10:03:52 +0000

    updated allauth and tested profiles

--- profiles/models.py
+++ profiles/models.py
@@ -13,6 +13,7 @@ class UserProfile(models.Model):
     user = models.OneToOneField(User, on_delete=models.CASCADE)
     default_phone_number = models.CharField(max_length=20, null=True, blank=True)
 
+
     def __str__(self):
         return self.user.username
 
