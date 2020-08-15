f36eea008795a0d22510834682eb72b304b141c2
heathergi85 <heathergi85@gmail.com>
2020-08-15 08:05:10 +0000

    products linked to checkout

--- checkout/forms.py
+++ checkout/forms.py
@@ -1,39 +1,23 @@
 from django import forms
 from .models import Order
+from datetime import datetime
 
 
-class OrderForm(forms.ModelForm):
-    class Meta:
-        model = Order
-        fields = ('full_name', 'email', 'phone_number',
-                  'street_address1', 'street_address2',
-                  'town_or_city', 'postcode', 'country',
-                  'county',)
 
-    def __init__(self, *args, **kwargs):
-        """
-        Add placeholders and classes, remove auto-generated
-        labels and set autofocus on first field
-        """
-        super().__init__(*args, **kwargs)
-        placeholders = {
-            'full_name': 'Full Name',
-            'email': 'Email Address',
-            'phone_number': 'Phone Number',
-            'postcode': 'Postal Code',
-            'town_or_city': 'Town or City',
-            'street_address1': 'Street Address 1',
-            'street_address2': 'Street Address 2',
-            'county': 'County, State or Locality',
-        }
 
-        self.fields['full_name'].widget.attrs['autofocus'] = True
-        for field in self.fields:
-            if field != 'country':
-                if self.fields[field].required:
-                    placeholder = f'{placeholders[field]} *'
-                else:
-                    placeholder = placeholders[field]
-                self.fields[field].widget.attrs['placeholder'] = placeholder
-            self.fields[field].widget.attrs['class'] = 'stripe-style-input'
-            self.fields[field].label = False
+year = int(datetime.now().strftime("%Y"))
+
+
+class MakePaymentForm(forms.Form):
+    """
+    Input for Strip Payment Collection
+    Default Expiration Year Range to current Year
+    """
+    MONTH_CHOICES = [(i, i) for i in range(1, 13)]
+    YEAR_CHOICES = [(i, i) for i in range(year, year + 20)]
+
+    credit_card_number = forms.CharField(label='Credit Card Number', required=False)
+    ccv = forms.CharField(label="Security Code", required=False)
+    expiry_month = forms.ChoiceField(choices=MONTH_CHOICES, required=False)
+    expiry_year = forms.ChoiceField(choices=YEAR_CHOICES, required=False)
+    stripe_id = forms.CharField(widget=forms.HiddenInput)
