1e0380c324169686d38e1db3f7f164d8812df90e
heathergi85 <heathergi85@gmail.com>
2020-08-26 08:44:46 +0000

    profile app / html page amended

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,9 +1,6 @@
 from django.urls import path
 from . import views
-from .webhooks import webhook
 
 urlpatterns = [
     path('<pk>', views.checkout, name='checkout'),
-
-    path('wh/', webhook, name='webhook'),
 ]
--- profiles/forms.py
+++ profiles/forms.py
@@ -1,6 +1,14 @@
 from django import forms
 from .models import UserProfile
 
+from crispy_forms.helper import FormHelper
+from crispy_forms.layout import Layout, Submit, Row, Column, HTML
+from django.contrib.auth.models import User
+from django.contrib.auth.forms import UserCreationForm, UserChangeForm
+from django.utils.safestring import mark_safe
+from django.db.utils import OperationalError
+from datetime import date
+
 
 class UserProfileForm(forms.ModelForm):
     class Meta:
@@ -14,15 +22,12 @@ class UserProfileForm(forms.ModelForm):
         """
         super().__init__(*args, **kwargs)
         placeholders = {
-            'default_phone_number': 'Phone Number',
-            'default_postcode': 'Postal Code',
-            'default_town_or_city': 'Town or City',
-            'default_street_address1': 'Street Address 1',
-            'default_street_address2': 'Street Address 2',
-            'default_county': 'County, State or Locality',
+            'user_name' : 'User Name',
+            'phone_number': 'Phone Number',
+            'email' : 'Email',
         }
 
-        self.fields['default_phone_number'].widget.attrs['autofocus'] = True
+        self.fields['phone_number'].widget.attrs['autofocus'] = True
         for field in self.fields:
             if field != 'default_country':
                 if self.fields[field].required:
@@ -32,3 +37,48 @@ class UserProfileForm(forms.ModelForm):
                 self.fields[field].widget.attrs['placeholder'] = placeholder
             self.fields[field].widget.attrs['class'] = 'border-black rounded-0 profile-form-input'
             self.fields[field].label = False
+
+class ProfileForm(forms.ModelForm):
+    """
+    Collect Updated Profile Info From User
+    """
+    class Meta:
+        model = UserProfile
+        fields = '__all__'
+        exclude = ['user']
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('username', css_class='form-group col-md-6 mb-0'),
+                Column('email', css_class='form-group col-md-6 mb-0'),
+                Column('phone_number', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            
+            Submit('submit', 'Save Changes'),
+            Submit('cancel', 'Cancel', css_class='btn btn-cancel')
+        )
+
+class UserUpdateForm(UserChangeForm):
+    """ Form to update User info username and email """
+    email = forms.EmailField()
+
+    class Meta:
+        model = User
+        fields = ['username', 'email' ]
+
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
+        self.helper = FormHelper()
+        self.helper.layout = Layout(
+            Row(
+                Column('username', css_class='form-group col-md-6 mb-0'),
+                Column('email', css_class='form-group col-md-6 mb-0'),
+                css_class='form-row'
+            ),
+            Submit('submit', 'Save Changes'),
+            Submit('cancel', 'Cancel', css_class='btn btn-cancel')
+        )
