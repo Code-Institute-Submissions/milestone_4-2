a6bd04c16c5531f848aa62fa6b442372a9d137ff
heathergi85 <heathergi85@gmail.com>
2020-08-28 09:42:56 +0000

    webhooks added

--- checkout/urls.py
+++ checkout/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from . import views
+from .webhooks import webhook
 
 urlpatterns = [
     path('<pk>', views.checkout, name='checkout'),
+    path('wh/', webhook, name='webhook'),
 ]
