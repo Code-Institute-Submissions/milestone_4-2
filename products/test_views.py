3ab8256c23bdb70fb4c5b4c90792a7d1b1a4956f
heathergi85 <heathergi85@gmail.com>
2020-08-30 07:34:01 +0000

    testing files deleted

--- products/test_views.py
+++
@@ -1,9 +0,0 @@
-from django.test import TestCase
-from .models import Training
-
-class TestProductViews(TestCase):
-
-    def test_get_product_page(self):
-        page = self.client.get("/products/")
-        self.assertEqual(page.status_code, 200)
-        self.assertTemplateUsed('products.html')
