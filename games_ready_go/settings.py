37261b5e786ab73217198253b53ffb2aa8bebc9c
heathergi85 <heathergi85@gmail.com>
2020-08-30 20:55:42 +0000

    postgres db setup, first stage of deployment to heroku

--- games_ready_go/settings.py
+++ games_ready_go/settings.py
@@ -12,6 +12,7 @@ https://docs.djangoproject.com/en/3.0/ref/settings/
 
 import os
 import env
+import dj_database_url
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -119,11 +120,10 @@ WSGI_APPLICATION = 'games_ready_go.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
+       'ENGINE': 'django.db.backends.sqlite3',
         'NAME': os.path.join(BASE_DIR, 'db.sqlite3'),
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.0/ref/settings/#auth-password-validators
