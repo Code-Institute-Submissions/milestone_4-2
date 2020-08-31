b2e45445724989dc829616e20180ff0142604de3
heathergi85 <heathergi85@gmail.com>
2020-08-31 09:40:04 +0000

    secret key added

--- games_ready_go/settings.py
+++ games_ready_go/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/3.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '!4soc)p+rk6_4jxw#g*)*z#h%p9@3qeg362(au5=#7o+jf8_-q'
+SECRET_KEY = os.environ.get('SECRET_KEY', 'some value if your key is not in the environment')
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
