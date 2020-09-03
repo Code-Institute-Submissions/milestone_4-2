13fc9e4d81f23c013c06bbf075b9014311c23185
heathergi85 <heathergi85@gmail.com>
2020-09-03 15:34:27 +0000

    checkout updated for heroku bug

--- checkout/forms.py
+++ checkout/forms.py
@@ -25,5 +25,5 @@ class OrderForm(forms.ModelForm):
     class Meta:
         model = Order
         fields = (
-            'user', 'first_name', 'last_name', 'email'
+            'user', 'email'
         )
