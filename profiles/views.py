9a715723d4fbc853fe3c64028d82cd86095a7cf9
heathergi85 <heathergi85@gmail.com>
2020-09-03 16:16:49 +0000

    profile views updated for heroku bug

--- profiles/views.py
+++ profiles/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render
 from django.contrib import messages
-from .forms import UserProfileForm, ProfileEditForm, UserEditForm
+from .forms import UserProfileForm
 from django.contrib.auth.decorators import login_required
+
 
 
 @login_required
@@ -19,20 +20,6 @@ def profile(request):
 @login_required
 def update_profile(request):
     if request.method == 'POST':
-        user_form = UserEditForm(instance=request.user,
-                                 data=request.POST)
-        profile_form = ProfileEditForm(
-                                        instance=request.user.userprofile,
-                                        data=request.POST,
-                                        files=request.FILES)
-        if user_form.is_valid() and profile_form.is_valid():
-            user_form.save()
-            profile_form.save()
-
-    else:
-        user_form = UserEditForm(instance=request.user)
-        profile_form = ProfileEditForm(
-                                    instance=request.user.profile)
-    return render(request,
-                  'profiles/update_profile.html',
-                  {'user_form': user_form, 'profile_form': profile_form})
+   
+        return render(request,
+                  'profiles/update_profile.html')
