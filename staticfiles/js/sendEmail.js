0b99619939ac93b8b6a9bc796a28af8dc0b7100b
heathergi85 <heathergi85@gmail.com>
2020-09-03 08:25:19 +0000

    collect static

---
+++ staticfiles/js/sendEmail.js
@@ -0,0 +1,17 @@
+function sendMail(contactForm) {
+    emailjs.send("gmail", "grg_contact_form", {
+        "from_name": contactForm.name.value,
+        "from_email": contactForm.emailaddress.value,
+        "message": contactForm.message.value
+    })
+    .then(
+        function(response) {
+            console.log("SUCCESS", response);
+        },
+        function(error) {
+            console.log("FAILED", error);
+        }
+    );
+    return render('index.html');
+}
+
